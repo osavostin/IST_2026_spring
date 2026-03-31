2917192da1f29e7a5888d03fcbd21a56cfbbb806
osavostin <mr.sans.san2@gmail.com>
2026-04-01 00:03:46 +0300

    Update rsa.py

--- homework-02/rsa.py
+++ homework-02/rsa.py
@@ -15,12 +15,7 @@ def is_prime(n: int) -> bool:
     """
     if n < 2:
         return False
-    if n == 2:
-        return True
-    if n % 2 == 0:
-        return False
-    
-    for i in range(3, int(n**0.5) + 1, 2):
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
@@ -35,7 +30,7 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    while b != 0:
+    while b:
         a, b = b, a % b
     return a
 
@@ -48,17 +43,21 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # Extended Euclidean Algorithm
-    original_phi = phi
-    x0, x1 = 0, 1
-    
-    while phi != 0:
-        quotient = e // phi
-        e, phi = phi, e % phi
-        x0, x1 = x1, x0 - quotient * x1
-    
-    # Make sure the result is positive
-    return x0 % original_phi
+    m0 = phi
+    y, x = 0, 1
+    if phi == 1:
+        return 0
+    while e > 1:
+        q = e // phi
+        t = phi
+        phi = e % phi
+        e = t
+        t = y
+        y = x - q * y
+        x = t
+    if x < 0:
+        x = x + m0
+    return x
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
@@ -67,45 +66,30 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
-    # n = pq
     n = p * q
-
-    # phi = (p-1)(q-1)
     phi = (p - 1) * (q - 1)
 
-    # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
 
-    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
 
-    # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
 
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
 
 
 def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
-    # Unpack the key into it's components
     key, n = pk
-    # Convert each letter in the plaintext to numbers based on
-    # the character using a^b mod m
     cipher = [(ord(char) ** key) % n for char in plaintext]
-    # Return the array of bytes
     return cipher
 
 
 def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
-    # Unpack the key into its components
     key, n = pk
-    # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
-    # Return the array of bytes as a string
+    plain = [chr((char**key) % n) for char in ciphertext]
     return "".join(plain)
 
 
